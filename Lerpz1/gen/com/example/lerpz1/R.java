da93975aecf7807d2e4481a37ceeac30c10a001c
alexpitzen <alexpitzen@gmail.com>
2013-01-30 12:36:29 -0600

    i don't know

--- Lerpz1/gen/com/example/lerpz1/R.java
+++ Lerpz1/gen/com/example/lerpz1/R.java
@@ -22,18 +22,18 @@ public final class R {
         public static final int map1=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f090004;
-        public static final int button2=0x7f090006;
-        public static final int button3=0x7f090003;
-        public static final int button4=0x7f090005;
-        public static final int button5=0x7f090002;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090003;
+        public static final int button3=0x7f090004;
+        public static final int button4=0x7f090002;
+        public static final int button5=0x7f090005;
         public static final int editText1=0x7f090008;
         public static final int expandableListView1=0x7f090009;
         public static final int gridView1=0x7f090000;
-        public static final int imageView1=0x7f090007;
+        public static final int imageView1=0x7f090006;
         public static final int menu_settings=0x7f09000b;
         public static final int switch1=0x7f09000a;
-        public static final int textView1=0x7f090001;
+        public static final int textView1=0x7f090007;
     }
     public static final class layout {
         public static final int activity_help=0x7f030000;
