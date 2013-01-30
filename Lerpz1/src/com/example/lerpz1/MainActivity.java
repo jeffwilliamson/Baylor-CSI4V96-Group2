8d775859107fc07d8589da3471146eefe4c21a97
alexpitzen <alexpitzen@gmail.com>
2013-01-30 12:21:41 -0600

    Can't rotate main screen

--- Lerpz1/src/com/example/lerpz1/MainActivity.java
+++ Lerpz1/src/com/example/lerpz1/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends NoRotateActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
